397854cae0e113feb79e8f0da4527c1301b3bac4
vivaansihmar <vivaansihmar@gmail.com>
2025-09-15 23:59:00 +0530

    fixes

--- app.py
+++ app.py
@@ -109,4 +109,7 @@ def index():
     return render_template("heatmap.html", message=message)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True, use_reloader=False)
+
